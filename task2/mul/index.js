8465f5b09ecc2448504df88ff92ac4b5b71576ea
Ghassan Alkhatib <85581306+gh49@users.noreply.github.com>
2024-03-26 01:04:18 +0000

    task2

---
+++ task2/mul/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.post('/mul', (req, res) => {
+    try{
+        const {X, Y} = req.body;
+        const Result = X * Y;
+        const response = {X: X, Y: Y, Result: Result};
+        res.send(response);
+    }
+    catch(error) {
+        res.send(error.message);
+    }
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Server is up on 3000');
+    }
+);
